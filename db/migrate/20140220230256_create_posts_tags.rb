4e4dd2147ac6c151dfa7a1b760b934bd0ef3e685
Joe Wilmoth <jbwilmoth@gmail.com>
2014-02-20 23:49:22 -0600

    added back migrations to include text for content rather than a string

--- db/migrate/20140220230256_create_posts_tags.rb
+++ db/migrate/20140220230256_create_posts_tags.rb
@@ -1,4 +1,9 @@
 class CreatePostsTags < ActiveRecord::Migration
   def change
+  	create_table :post_tags do |t|
+  	  t.belongs_to :post
+  	  t.belongs_to :tag
+  	  t.timestamps
+  	end
   end
 end
